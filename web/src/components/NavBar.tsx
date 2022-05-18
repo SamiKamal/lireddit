da3dfc930d31621341083449455ae57af43e27ce
Sami Ibrahim <sami-dev@hotmail.com>
2022-05-18 20:17:22 +0300

    feat-web: Show user on navbar

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,21 +1,42 @@
-import { Box, Flex, Link } from '@chakra-ui/react';
-import React from 'react';
+import { Box, Button, Flex, Link, Spinner } from '@chakra-ui/react';
+import React, { useEffect } from 'react';
 import NextLink from 'next/link';
+import { useMeQuery } from '../generated/graphql';
 
 interface NavBarProps {
 
 }
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
+    const [{data, fetching}] = useMeQuery();
+    let body = null;
+
+    // Found a user
+    if (data?.me?.id) {
+        body = (
+            <Flex>
+                <Box mr={2}>Hey {data.me.username}</Box>
+                <Button variant="link">Logout</Button>
+            </Flex>
+        )
+        // No user found
+    } else {
+        body = (<>
+                <NextLink href="/login">
+                    <Link mr={2}>Login</Link>
+                </NextLink>
+                <NextLink href="/register">
+                    <Link>Register</Link>
+                </NextLink>
+                </>)
+    }
+    
         return (
             <Flex bg="tomato" p={4} ml="auto">
                 <Box ml="auto">
-                    <NextLink href="/login">
-                        <Link mr={2}>Login</Link>
-                    </NextLink>
-                    <NextLink href="/register">
-                        <Link>Register</Link>
-                    </NextLink>
+                    {fetching ? <Spinner/> : (
+                        body
+                    )}
                 </Box>
             </Flex>
         );
