c97401d6d9d70c4585ac4750186526503d2f82df
Sami Ibrahim <sami-dev@hotmail.com>
2022-05-18 20:34:33 +0300

    fix: URQL cache updates

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -32,7 +32,7 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
     }
     
         return (
-            <Flex bg="tomato" p={4} ml="auto">
+            <Flex bg="tan" p={4} ml="auto">
                 <Box ml="auto">
                     {fetching ? <Spinner/> : (
                         body
