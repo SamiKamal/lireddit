43c6e252be388ef3accc1246ac63e9f08556a114
Sami Ibrahim <sami-dev@hotmail.com>
2022-05-23 21:06:33 +0300

    feat-web: create new Layout component

---
+++ web/src/components/Layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavBar } from './NavBar';
+import { Wrapper, WrapperProps } from './Wrapper';
+
+interface LayoutProps extends WrapperProps {
+
+}
+
+export const Layout: React.FC<LayoutProps> = ({variant, children}) => {
+        return (
+            <>
+                <NavBar/>
+                <Wrapper variant={variant}>
+                    {children}
+                </Wrapper>
+            </>
+        );
+}
