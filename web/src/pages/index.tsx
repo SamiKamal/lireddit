2fed2b99bd7d489ec2d3b1077e3448767a499bde
Sami Ibrahim <sami-dev@hotmail.com>
2022-05-24 19:17:40 +0300

    refactor-web: redesign and refactor home page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,33 +1,45 @@
-import { Link } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
 import { Layout } from "../components/Layout";
-import { NavBar } from "../components/NavBar"
 import { createUrqlClient } from "../utils/createUrqlClient";
 import NextLink from 'next/link'
 import { usePostsQuery } from "../generated/graphql";
 
 const Index = () => {
-  const [{data,fetching}] = usePostsQuery({
+  const [{data,fetching, error}] = usePostsQuery({
     variables: {
       limit: 10
     }
   });
 
+  if (error) {
+    console.log(error);
+    return <div>An error has occured, check the console.</div>
+  }
+
   return (<Layout>
-    <NextLink href="/create-post">
-      <Link>
-        create post
-      </Link>
-    </NextLink>
+    <Flex mb={4} align="center">
+      <Heading>liReddiT</Heading>
+      <NextLink href="/create-post">
+        <Link textDecoration="underline" ml="auto">
+          create post
+        </Link>
+      </NextLink>
+    </Flex>
     <div>
-      Hello World
-      {fetching ? 'loading...' : (
-        data?.posts.map((post, i) => (
-          <ul>
-            <li key={i}>{post.title}</li>
-          </ul>
-        ))
+      {fetching && !data ? 'loading...' : (
+        <Stack spacing={8}>
+          {data?.posts.map((post) => (
+            <Box key={post.id} p={5} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{post.title}</Heading>
+              <Text mt={4}>{post.textSnippet}</Text>
+            </Box>
+          ))}
+        </Stack>
       )}
+      {data ? (<Flex>
+        <Button isLoading={fetching} m="auto" my={8}>load more</Button>
+      </Flex>) : ''}
     </div>
   </Layout>)
 }
